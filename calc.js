96959a1aa6a9964a8951f068acfdc55e2ad4390b
Jeffbanawa <jeffrey.banawa@gmail.com>
2019-02-15 00:42:15 -0700

    Updated Styling

--- calc.js
+++ calc.js
@@ -1,13 +1,16 @@
 function insert(num) {
-    document.form.textview.value = document.form.textview.value+num
+    document.form.textview.value = document.form.textview.value+num;
 }
 
 function equal() {
-    const input = document.form.textview.value
+    const input = document.form.textview.value;
     if (input) {
-        document.form.textview.value = eval(document.form.textview.value)
+        var prevText = document.form.textview.value+"=";
+        document.form.textview.value = eval(document.form.textview.value);
+        var prev = document.getElementById("answer");
+        prev.innerHTML = prevText;
     }
 }
 function ac() {
-    document.form.textview.value = ""
+    document.form.textview.value = "";
 }
